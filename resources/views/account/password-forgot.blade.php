ece73de3bdbaa5de8d06f7e5b1ad40ab21e3ce19
Jonathan La Mela <jonathan.la.mela@gmail.com>
2023-04-26 14:37:26 +0200

    password forgot tailwind

--- resources/views/account/password-forgot.blade.php
+++ resources/views/account/password-forgot.blade.php
@@ -3,49 +3,55 @@
 @section('title', 'Recupera password')
 
 @section('topbarLeft')
-<x-global-search-form></x-global-search-form>
+    <x-global-search-form></x-global-search-form>
 @endsection
 
 @section('topbarRight')
-<x-cart-button></x-cart-button>
-<x-account-manage></x-account-manage>
+    <x-cart-button></x-cart-button>
+    <x-account-manage></x-account-manage>
 @endsection
 
 @section('header')
-<x-header></x-header>
+    <x-header></x-header>
 @endsection
 
 @section('nav')
-
+    <div class="flex h-16">
+        <ol class="breadcrumb-container">
+            <li>
+                <a class="breadcrumb-link" href="{{ route('login') }}">
+                    Profilo
+                </a>
+            </li>
+            <li>::</li>
+            <li>Recupera password</li>
+        </ol>
+    </div>
 @endsection
 
 @section('content')
-<x-breadcrumb>
-    <li class="breadcrumb-item">
-        <a class='text-light' href="{{route("account.dashboard")}}">Profilo</a>
-    </li>
-    <li class="breadcrumb-item active text-light" aria-current="page">Cambia password</li>
-</x-breadcrumb>
-<x-messages></x-messages>
-<div class="row g-0 d-flex flex-grow-1">
-    <div class="col-lg-12 p-4 d-flex flex-column align-items-center justify-content-center">
-        <form method="post" class="col-lg-4" action="{{route('password.email')}}">
-            <p>Compila il form per cambiare la tua password</p>
 
+    <div class="px-4 pt-4">
+        <x-messages></x-messages>
+    </div>
+    <div class='flex flex-grow justify-center items-center'>
+
+        <form method="post" class="w-full p-16 md:p-0 md:w-1/2 lg:w-1/3 flex flex-col space-y-2"
+            action="{{ route('password.email') }}">
             @csrf
-            <div class="mb-3 form-group">
-                <label for="staticEmail" class="form-label">Email</label>
-                <input type="text" name="email" class="form-control @error('email') is-invalid @enderror" id="staticEmail">
+            <p>Compila il form per recuperare la tua password</p>
+            <div class="flex flex-col space-y-2">
+                <label class="form-label">Email</label>
+                <input type="text" name="email" class="text-input @error('email') text-input-invalid @enderror" />
                 @error('email')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
                 @enderror
             </div>
-            <div class="mb-3">
-                <button type="submit" class="btn btn-success">Recupera la password</button>
+            <div class="flex flex-row space-x-2 pt-4">
+                <button type="submit" class="btn-primary">Recupera password</button>
             </div>
         </form>
     </div>
-</div>
 @endsection
