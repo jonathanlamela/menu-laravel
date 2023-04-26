d85cacc46715d7639816980b1baebe8c7bd7e6c6
Jonathan La Mela <jonathan.la.mela@gmail.com>
2023-04-26 14:33:23 +0200

    password change tailwind

--- resources/views/account/password-change.blade.php
+++ resources/views/account/password-change.blade.php
@@ -3,68 +3,77 @@
 @section('title', 'Recupera password')
 
 @section('topbarLeft')
-<x-global-search-form></x-global-search-form>
+    <x-global-search-form></x-global-search-form>
 @endsection
 
 @section('topbarRight')
-<x-cart-button></x-cart-button>
-<x-account-manage></x-account-manage>
+    <x-cart-button></x-cart-button>
+    <x-account-manage></x-account-manage>
 @endsection
 
 @section('header')
-<x-header></x-header>
+    <x-header></x-header>
 @endsection
 
 @section('nav')
-
+    <div class="flex h-16">
+        <ol class="breadcrumb-container">
+            <li>
+                <a class="breadcrumb-link" href="{{ route('account.dashboard') }}">
+                    Profilo
+                </a>
+            </li>
+            <li>::</li>
+            <li>Cambia password</li>
+        </ol>
+    </div>
 @endsection
 
 @section('content')
-<x-breadcrumb>
-    <li class="breadcrumb-item">
-        <a class='text-light' href="{{route("account.dashboard")}}">Profilo</a>
-    </li>
-    <li class="breadcrumb-item active text-light" aria-current="page">Cambia password</li>
-</x-breadcrumb>
-<x-messages></x-messages>
-<div class="row g-0 d-flex flex-grow-1">
-    <div class="col-lg-12 p-4 d-flex flex-column align-items-center justify-content-center">
-        <form class="row col-lg-4" method="post" action="{{route('user-password.update')}}">
-            <p>Compila il form per cambiare la tua password</p>
 
+    <div class="p-8">
+        <x-messages></x-messages>
+        <div class="w-full">
+            <h4 class='text-2xl antialiased font-sans'>Cambia password</h4>
+        </div>
+        <form method="post" class="w-full md:p-0 md:w-1/2 lg:w-1/3 flex flex-col space-y-2"
+            action="{{ route('user-password.update') }}">
             @csrf
             @method('put')
-            <div class="mb-3 form-group">
-                <label for="inputPassword" class="form-label">Password attuale</label>
-                <input type="password" name="current_password" class="form-control @error('current_password') is-invalid @enderror" />
+            <div class="flex flex-col space-y-2">
+                <label class="form-label">Password attuale</label>
+                <input type="password" name="current_password"
+                    class="text-input @error('current_password') text-input-invalid @enderror" />
                 @error('current_password')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
                 @enderror
             </div>
-            <div class="mb-3 form-group">
-                <label for="inputPassword" class="form-label">Password</label>
-                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror" />
+            <div class="flex flex-col space-y-2">
+                <label class="form-label">Password</label>
+                <input type="password" name="password" class="text-input @error('password') text-input-invalid @enderror" />
                 @error('password')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
                 @enderror
             </div>
-            <div class="mb-3 form-group">
-                <label for="inputPassword" class="form-label">Conferma password</label>
-                <input type="password" name="password_confirmation" class="form-control @error('password_confirmation') is-invalid @enderror">
+            <div class="flex flex-col space-y-2">
+                <label class="form-label">Conferma password</label>
+                <input type="password" name="password_confirmation"
+                    class="text-input @error('password_confirmation') text-input-invalid @enderror" />
                 @error('password_confirmation')
-                <div class="invalid-feedback">
-                    {{ $message }}
-                </div>
+                    <div class="invalid-feedback">
+                        {{ $message }}
+                    </div>
                 @enderror
             </div>
-            <div class="mb-3 form-group">
-                <button type="submit" class="btn btn-success">Cambia password</button>
+            <div class="flex flex-row space-x-2">
+                <button type="submit" class="btn-primary">
+                    <span>Cambia password</span>
+                </button>
             </div>
         </form>
     </div>
-</div>
 @endsection
